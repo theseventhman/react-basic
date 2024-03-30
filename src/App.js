a570a7e404912e21b1e20eb5e2e7e86d80341d37
tcn741 <tianchenlko@126.com>
2024-03-30 18:22:35 +0800

    day-01-05 列表渲染

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ function getName() {
   return 'jack'
 }
 
+const list = [
+  {id:1001, name:"Vue"},
+  {id:1002, name:"React"},
+  {id:1003, name:"Angular"}
+]
+
 function App() {
   return (
     <div className="App">
       this is App
-      {/* 使用引号传递字符串 */}
-      {'this is message'}
-      {/*  识别js变量 */}
-      {count}
-      {/* 函数调用 */}
-      {getName()}
-      {/* 方法调用 */}
-      {new Date().getDate()}
-      {/* 使用js对象 */}
-      <div style={{color:'red'}}>this is a div </div>
+     {/* 渲染列表 */}
+     <ul>
+       {list.map(item=> <li key={item.id}>{item.name}</li>)}
+     </ul>
     </div>
   );
 }
