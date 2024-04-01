9f2e4bf798aebc862b4aa6bbb3df51d6d8120e8b
tcn741 <tianchenlko@126.com>
2024-04-01 20:03:34 +0800

    day1-10-useState基础使用

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 
 // 定义组件
 // App -> index.js -> public/index.html(root)
-const Button = () =>{
-  //业务逻辑组件逻辑
-  return <button>click me!</button>
-}
+// useState 实现一个计数器按钮
+import { useState } from "react";
 function App() {
+  // 1.调用 useState添加一个状态变量
+  // count 状态变量
+  // setCount 修改装填变量的方法
+  const[count,setCount] = useState(0)
+
+  // 2. 点击事件回调
+  const handleClick = () =>{
+    // 作用: 1.用传入的值修改count
+    // 2.重新使用新的count渲染UI
+    setCount(count+1)
+  }
+
   return (
     <div className="App">
-      {/* 使用组件 （渲染组件) */}
-      {/* 自闭合 */}
-      <Button/>
-      <Button></Button>
-      
-    
+     <button onClick={handleClick}>{count}</button>
     </div>
   )
 }
