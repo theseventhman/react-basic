89a9f58ffbd19bf3ba35336ee8040ff6af292758
tcn741 <tianchenlko@126.com>
2024-04-03 13:46:29 +0800

    day2-09-子传父

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 // 父传子
 // 1.父组件传递数据 子组件标签身上绑定属性
 
-function Son(props){
-  // props: 对象里面包含了父组件传递过来的所有的数据
-  return <div>this is son, {props.name}</div>
+import { useState } from "react"
+
+function Son({onGetSonMsg}){
+  // Son组件中的数据
+  const sonMsg = 'this is son msg'
+  return (
+    <div>
+      this is Son 
+      <button onClick={()=> onGetSonMsg(sonMsg)}></button>
+    </div>
+    )
 }
 
 function App(){
-  const name = 'this is app name'
+  const[msg,setMsg] = useState('')
+  const getMsg = (msg) =>{
+    console.log(msg);
+    setMsg(msg);
+  }
   return (
     <div>
-      <Son name={name}/>
+      this is App, {msg}
+      <Son onGetSonMsg={getMsg}/>
     </div>
   )
 }
